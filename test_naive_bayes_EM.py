9c3e09a3a93d85bb6bcba3697404055c70cb2cce
cozyberry <whu.amadeus@gmail.com>
2013-07-16 18:05:17 +0200

    can test different datasets

--- test_naive_bayes_EM.py
+++ test_naive_bayes_EM.py
@@ -3,8 +3,11 @@
 import naive_bayes_EM 
 import os,sys,getopt,random,csv
 import numpy as np
+from data_utilities import countData,readData,random_gen
+
 
 DATAPATH="/home/wei/data_processing/data/car/car.data"
+DATAPATHS=['data/car/car.unacc.data','data/car/car.acc.data','data/car/car.good.data','data/car/car.vgood.data']
 ITERCN = 20
 ITERSN = 1
 _VERBOSE = False
@@ -16,40 +19,18 @@ OUTPUTDIR ='/home/wei/share/nbem/outputs/'
 LTYPE = 0
 
 def usage():
-    print "%s [-c type_of_likelihood] [-n nonstochastic_iteration_times] [-s stochastic_iteration_times] [-v] [-o] [-d] [-k initial_clustering_number] [-i initial_method]"%sys.argv[0]
+    print "%s [-c type_of_likelihood] [-n nonstochastic_iteration_times] [-s stochastic_iteration_times] [-v] [-o] [-d] [-k initial_clustering_number] [-i initial_method] [-a] [filenames]"%sys.argv[0]
     print "     [-c type_of_likelihood]: 0 for normal likelihood;1 for classification likelihood;2 for naive bayesian network. 0 By default"
     print "     [-n iteration_times]: set nonstochastic iteration times for EM method. Default is 20"
     print "     [-s stochastic_iteration_times]: set stochastic iteration times for EM method. Default is 1"
     print "     [-v]: set verbose mode. Print other detail infomation"
-    print "     [-l]: set objective of maximization of log likelihood; by default maximiation of score. Need to analysize further"
     print "     [-o]: output predicted class label and original label as well for further analysis"
     print "     [-d]: output file name with time stamp, only valid together with -o option"
-    print "     [-p]: set partition mode."
     print "     [-k initial_clustering_number]: set an initial clustering number for EMNB or ECMNB."
     print """     [-i initial_method]:            set initial methods of label for EM method. 
                                      initial_method: an integer specifying the initial method 
                                      0: uniform initialization
                                      1: k points methods. Refer to PHAM2009 'Unsupervised training of bayesian networks for data clustering'"""
-
-def readData(filename):
-    if not os.path.exists(filename):
-        print "I can't find this file: %s"%filename
-        sys.exit(1)
-
-    datareader = csv.reader(open(filename,'r'))
-    ct = 0;
-    for row in datareader:
-        ct = ct+1
-
-    datareader = csv.reader(open(filename,'r'))
-    data = np.array(-1*np.ones((ct,7),float),object);
-    k=0;
-
-    for row in datareader:
-        data[k,:] = np.array(row)
-        k = k+1;
-
-    return data
 
 
 def main(argv):
@@ -69,6 +50,7 @@ def main(argv):
     global LOGDIR 
     initMethod = 0
     _PARTITION = False
+    _attr = False
     numc = 4
     for opt,arg in opts:
         if opt in ("-h","--help"):           
@@ -96,16 +78,24 @@ def main(argv):
             numc = int(arg)
         elif opt in ("-i"):
             initMethod = int(arg)
+        elif opt in ("-a"):
+            _attr=True
 
     if LTYPE == 0:
         random.seed()
-        rdata = readData(DATAPATH)
+        if args !="":
+            DATAPATHS = args
+        rdata = random_gen(DATAPATHS,ratios='10:5:1:1',_random=True)
         nbem=naive_bayes_EM.MultinomialNBEM()
         nbem.setVerbose(_VERBOSE)
         if _OUTPUT:
             nbem.setOutput(OUTPUTDIR)
-        xdata_ml,ydata=nbem.fit_transformRaw(rdata,True,ATTRIBUTES)
-        nbem.build(numc,xdata_ml,ITERSN,ITERCN,initMethod,_DATE)
+        if _attr:
+            xdata_ml,ydata=nbem.fit_transformRaw(rdata,True,ATTRIBUTES)
+        else:
+            xdata_ml,ydata=nbem.fit_transformRaw(rdata,True)
+            
+        nbem.build(numc,xdata_ml,ITERSN,ITERCN,initMethod,_DATE,ydata)
         nbem.testModel(xdata_ml,ydata,_DATE)
     else:
         raise ValueError("Oh I just know NBEM. The corresponding LTYPE is 0")
